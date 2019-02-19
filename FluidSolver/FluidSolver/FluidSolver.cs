0586f91dbc0c995be741cdfa30a01a07dce6d097
Zhiquan Wang <zhiquan.wzq@gmail.com>
2019-02-19 15:03:14 +0800

    Add Function:ComputePressure & ComputePressureForceAcceleration

--- FluidSolver/FluidSolver/FluidSolver.cs
+++ FluidSolver/FluidSolver/FluidSolver.cs
@@ -7,52 +7,56 @@ using System.Numerics;
 namespace FluidSolver {
     public class FluidSolver {
         private List<Particle> _particleList;
-        private Vector _containerVolume;
+        private Vector3 _containerVolume;
         private int _particleNum;
-        private double _particleMass; //set particle
-        private double _restDensity; // set particle \rho_0
-        private double _viscosityCoefficient; // mu
-        private double _coreRadius; // h
-        private double _gasConstant; // k 
-        private double _tensionCoefficient; //sigma
-        private double _gravityAcceleration; //g
+        private float _particleMass; //set particle
+        private float _restDensity; // set particle \rho_0
+        private float _viscosityCoefficient; // mu
+        private readonly float _coreRadius; // h
+        private float _gasConstant; // k 
+        private float _tensionCoefficient; //sigma
+        private float _gravityAcceleration; //g
        
-        public FluidSolver(Vector inContainerVolume,double inCoreRadius) {
+        public FluidSolver(Vector3 inContainerVolume,float inCoreRadius) {
             this._containerVolume = inContainerVolume;
             this._particleList = new List<Particle>();
             this._particleNum = 0;
             this._coreRadius = inCoreRadius;
-            var tmpRestriction = new Vector(100, 100, 50);
+            var tmpRestriction = new Vector3(100, 100, 50);
             InitParticles(1000,1,tmpRestriction);
-            CalDensity();
-            TestCalDensity();
+            TestInitParticles();
+//            ComputeDensity();
+//            TestCalDensity();
         }
 
-        public void InitParticles(int inParticleNum,double inParticleMass,Vector inPosRestriction){
-            double posInterval = Math.Pow(inPosRestriction.X * inPosRestriction.Y * inPosRestriction.Z/inParticleNum,1/3f);
+        public void InitParticles(int inParticleNum,float inParticleMass,Vector3 inPosRestriction){
+            float posInterval = (float) Math.Pow(inPosRestriction.X * inPosRestriction.Y * inPosRestriction.Z/inParticleNum,1/3f);
             int xNum = (int) (inPosRestriction.X / posInterval);
             int yNum = (int) (inPosRestriction.Y / posInterval);
             int zNum = (int) (inPosRestriction.Z / posInterval);
             this._particleNum += xNum * yNum * zNum;
+            int counter = 0;
             for (int i = 0; i < xNum; ++i) {
                 for (int j = 0; j < yNum; ++j) {
                     for (int k = 0; k < zNum; ++k) {
-                        var tmpPos = new Vector(posInterval*i,posInterval*j,posInterval*k);
-                        this._particleList.Add(new Particle(inParticleMass,tmpPos));
+                        var tmpPos = new Vector3(posInterval*i,posInterval*j,posInterval*k);
+                        this._particleList.Add(new Particle(counter++,inParticleMass,tmpPos));
                     }
                 }
             }
+
+
         }
 
-        private void CalDensity() {
-            double hSquare = this._coreRadius * this._coreRadius;
-            double tmpCoefficient = 315 / (64 * Math.PI * Math.Pow(hSquare, 9));
+        private void ComputeDensity() {
+            float hSquare = this._coreRadius * this._coreRadius;
+            float tmpCoefficient = (float) (315 / (64 * Math.PI * Math.Pow(hSquare, 9)));
             foreach (var pI in this._particleList) {
-                double tmpDensity = 0;
+                float tmpDensity = 0;
                 foreach (var pJ in this._particleList) {
-                    double tmpDisSqu = Vector.DistanceSquare(pI.Position, pJ.Position);
+                    float tmpDisSqu = Vector3.DistanceSquared(pI.Position, pJ.Position);
                     if (tmpDisSqu < hSquare) {
-                        tmpDensity += Math.Pow(hSquare - tmpDisSqu, 3);
+                        tmpDensity += (float )Math.Pow(hSquare - tmpDisSqu, 3);
                     }
                 }
 
@@ -61,11 +65,32 @@ namespace FluidSolver {
             }
         }
 
+        private void ComputePressure() {
+            foreach (var p in this._particleList) {
+                p.Pressure = this._gasConstant * (p.Density - _restDensity);
+            }
+        }
+
+        private void ComputePressureForceAcceleration() {
+            float tmpCoefficient =  (float) (45 / (Math.PI * Math.Pow(this._coreRadius, 6)));
+            foreach (var pI in this._particleList) {
+                var tmpVec = new Vector3();
+                foreach (var pJ in this._particleList) {
+                    float tmpDis = Vector3.Distance(pI.Position, pJ.Position);
+                    if (tmpDis < this._coreRadius) {
+                        tmpVec += (float) ((pI.Pressure + pJ.Pressure) / (2 * pI.Density * pJ.Density) *
+                                           Math.Pow(this._coreRadius - tmpDis, 2)) *
+                                  Vector3.Normalize(pI.Position - pJ.Position);
+                    }
+                }
+                tmpVec *= pI.Mass * tmpCoefficient * tmpVec;
+            }
+        }
         public void TestInitParticles() {
             Console.WriteLine(this._particleList.Count);
             int counter = 0;
             foreach (var p in this._particleList) {
-                Console.WriteLine(counter ++ +" "+p.Position.ToString());
+                Console.WriteLine(p.Index+" "+p.Position.ToString());
             }
         }
 
@@ -77,66 +102,71 @@ namespace FluidSolver {
     }
 
     public class Particle {
-        
-        private double _mass;
-        private Vector _position;
-        private double _density;
-        private double _pressure;
-        private Vector _velocity;
-        private Vector _acceleration;
-        private Vector _pressureForce;
-        private Vector _viscosityForce;
-
+        private int _index = 0;
+        private float _mass = 0;
+        private Vector3 _position = new Vector3();
+        private float _density = 0;
+        private float _pressure = 0;
+        private Vector3 _velocity = new Vector3();
+        private Vector3 _totalAcceleration = new Vector3();
+        private Vector3 _pressureForceAcceleration = new Vector3();
+        private Vector3 _viscosityForceAcceleration = new Vector3();
         
         public Particle (){
 
         }
         
-        public Particle(double inMass,Vector inPosition){
-            this._mass = inMass;
+        public Particle(int inIndex, float inMass,Vector3 inPosition) {
+            this.Index = inIndex;
+            this.Mass = inMass;
             this._position = inPosition;
-            this._density = 0;
         }
 
 
-        public double Mass {
+        public int Index {
+            get => _index;
+            set => _index = value;
+        }
+
+        public float Mass {
             get => _mass;
             set => _mass = value;
         }
 
-        public Vector Position {
+        public Vector3 Position {
             get => _position;
             set => _position = value;
         }
 
-        public double Density {
+        public float Density {
             get => _density;
             set => _density = value;
         }
 
-        public double Pressure {
+        public float Pressure {
             get => _pressure;
             set => _pressure = value;
         }
 
-        public Vector Velocity {
+        public Vector3 Velocity {
             get => _velocity;
             set => _velocity = value;
         }
 
-        public Vector Acceleration {
-            get => _acceleration;
-            set => _acceleration = value;
+        public Vector3 TotalAcceleration {
+            get => _totalAcceleration;
+            set => _totalAcceleration = value;
         }
 
-        public Vector PressureForce {
-            get => _pressureForce;
-            set => _pressureForce = value;
+        public Vector3 PressureForceAcceleration {
+            get => _pressureForceAcceleration;
+            set => _pressureForceAcceleration = value;
         }
 
-        public Vector ViscosityForce {
-            get => _viscosityForce;
-            set => _viscosityForce = value;
+        public Vector3 ViscosityForceAcceleration {
+            get => _viscosityForceAcceleration;
+            set => _viscosityForceAcceleration = value;
         }
+ 
     }
 }
--- FluidSolver/FluidSolver/Program.cs
+++ FluidSolver/FluidSolver/Program.cs
@@ -5,7 +5,7 @@ namespace FluidSolver {
         static void Main(string[] args) {
 
             Console.WriteLine("Start Generate Particles");
-            var fluidSolver = new FluidSolver(new Vector(100,100,100),30);
+            var fluidSolver = new FluidSolver(new Vector(100,100,100),0.01f);
             
             //fluidSolver.TestInitParticles();
         }
