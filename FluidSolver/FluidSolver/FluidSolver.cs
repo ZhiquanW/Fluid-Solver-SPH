3e1e2efc36dee8315aaaa5b79f4cef8593113e57
Zhiquan Wang <zhiquan.wzq@gmail.com>
2019-02-23 21:01:51 +0800

    FluidSolver:Functions:ComputeSurfaceTension

--- FluidSolver/FluidSolver/FluidSolver.cs
+++ FluidSolver/FluidSolver/FluidSolver.cs
@@ -17,21 +17,22 @@ namespace FluidSolver {
         private float _gasConstant; // k 
         private float _tensionCoefficient; //sigma
         private float _gravityAcceleration; //g
-       
-        public FluidSolver(Vector3 inContainerVolume,float inCoreRadius) {
+
+        public FluidSolver(Vector3 inContainerVolume, float inCoreRadius) {
             this._containerVolume = inContainerVolume;
             this._particleList = new List<Particle>();
             this._particleNum = 0;
             this._coreRadius = inCoreRadius;
             var tmpRestriction = new Vector3(100, 100, 50);
-            InitParticles(1000,1,tmpRestriction);
+            InitParticles(1000, 1, tmpRestriction);
             TestInitParticles();
 //            ComputeDensity();
 //            TestCalDensity();
         }
 
-        public void InitParticles(int inParticleNum,float inParticleMass,Vector3 inPosRestriction){
-            float posInterval = (float) Math.Pow(inPosRestriction.X * inPosRestriction.Y * inPosRestriction.Z/inParticleNum,1/3f);
+        public void InitParticles(int inParticleNum, float inParticleMass, Vector3 inPosRestriction) {
+            float posInterval =
+                (float) Math.Pow(inPosRestriction.X * inPosRestriction.Y * inPosRestriction.Z / inParticleNum, 1 / 3f);
             int xNum = (int) (inPosRestriction.X / posInterval);
             int yNum = (int) (inPosRestriction.Y / posInterval);
             int zNum = (int) (inPosRestriction.Z / posInterval);
@@ -40,13 +41,11 @@ namespace FluidSolver {
             for (int i = 0; i < xNum; ++i) {
                 for (int j = 0; j < yNum; ++j) {
                     for (int k = 0; k < zNum; ++k) {
-                        var tmpPos = new Vector3(posInterval*i,posInterval*j,posInterval*k);
-                        this._particleList.Add(new Particle(counter++,inParticleMass,tmpPos));
+                        var tmpPos = new Vector3(posInterval * i, posInterval * j, posInterval * k);
+                        this._particleList.Add(new Particle(counter++, inParticleMass, tmpPos));
                     }
                 }
             }
-
-
         }
 
         private void ComputeDensity() {
@@ -57,9 +56,10 @@ namespace FluidSolver {
                 foreach (var pJ in this._particleList) {
                     float tmpDisSqu = Vector3.DistanceSquared(pI.Position, pJ.Position);
                     if (tmpDisSqu < hSquare) {
-                        tmpDensity += (float )Math.Pow(hSquare - tmpDisSqu, 3);
+                        tmpDensity += (float) Math.Pow(hSquare - tmpDisSqu, 3);
                     }
                 }
+
                 pI.Density = pI.Mass * tmpCoefficient * tmpDensity;
             }
         }
@@ -71,7 +71,7 @@ namespace FluidSolver {
         }
 
         private void ComputePressureForceAcceleration() {
-            float tmpCoefficient =  (float) (45 / (Math.PI * Math.Pow(this._coreRadius, 6)));
+            float tmpCoefficient = (float) (45 / (Math.PI * Math.Pow(this._coreRadius, 6)));
             foreach (var pI in this._particleList) {
                 var tmpVec = new Vector3();
                 foreach (var pJ in this._particleList) {
@@ -82,12 +82,14 @@ namespace FluidSolver {
                                   Vector3.Normalize(pI.Position - pJ.Position);
                     }
                 }
+
                 pI.PressureForceAcceleration = pI.Mass * tmpCoefficient * tmpVec;
             }
         }
 
         private void ComputeViscosityForceAcceleration() {
-            float tmpCoefficient = (float) (this._viscosityCoefficient*45 / (Math.PI * Math.Pow(this._coreRadius, 6)));
+            float tmpCoefficient =
+                (float) (this._viscosityCoefficient * 45 / (Math.PI * Math.Pow(this._coreRadius, 6)));
             foreach (var pI in this._particleList) {
                 var tmpVec = new Vector3();
                 foreach (var pJ in this._particleList) {
@@ -96,6 +98,7 @@ namespace FluidSolver {
                         tmpVec += (pI.Velocity - pJ.Velocity) / (pI.Density * pJ.Density) * this._coreRadius * tmpDis;
                     }
                 }
+
                 pI.ViscosityForceAcceleration = pI.Mass * tmpCoefficient * tmpVec;
             }
         }
@@ -107,29 +110,40 @@ namespace FluidSolver {
         }
 
         private void ComputeSurfaceTensionAcceleration() {
-            float tmpCoefficient = (float) (-945 / (32 * Math.PI * Math.Pow(this._coreRadius,9)));
+            float tmpCoefficient = (float) (-945 / (32 * Math.PI * Math.Pow(this._coreRadius, 9)));
             float tmpCoefficientLaplacian = (float) (945 / (8 * Math.PI * Math.Pow(this._coreRadius, 9)));
+            float hSquared = (float) Math.Pow(this._coreRadius, 2);
             foreach (var pI in this._particleList) {
                 Vector3 colorFieldGradient = new Vector3();
                 float colorFieldLaplacian = 0;
                 foreach (var pJ in this._particleList) {
+                    float rSquared = Vector3.DistanceSquared(pI.Position, pJ.Position);
                     float tmpDis = Vector3.Distance(pI.Position, pJ.Position);
                     if (tmpDis < this._coreRadius) {
-                        colorFieldGradient += (float) (1 / pI.Density *
-                                                       Math.Pow(
-                                                           Math.Pow(this._coreRadius, 2) -
-                                                           Vector3.DistanceSquared(pI.Position, pJ.Position), 2)) *
-                                              (pI.Position - pJ.Position); 
-                        colorFieldLaplacian += 1/pI.Density
+                        colorFieldGradient += (float) (1 / pI.Density * Math.Pow(hSquared - rSquared, 2)) *
+                                              (pI.Position - pJ.Position);
+                        colorFieldLaplacian += 1 / pI.Density * (hSquared - rSquared) *
+                                               (rSquared - 3 / 4 * (hSquared - rSquared));
                     }
                 }
+
+                pI.SurfaceTensionAcceleration = -1 * _tensionCoefficient * pI.Mass * tmpCoefficientLaplacian /
+                                                pI.Density * Vector3.Normalize(colorFieldGradient);
             }
         }
+
+        private void ComputeTotalAcceleration() {
+            foreach (var pI in this._particleList) {
+                pI.TotalAcceleration = pI.GravityAcceleration + pI.PressureForceAcceleration +
+                                       pI.ViscosityForceAcceleration + pI.SurfaceTensionAcceleration;
+            }
+        }
+
         public void TestInitParticles() {
             Console.WriteLine(this._particleList.Count);
             int counter = 0;
             foreach (var p in this._particleList) {
-                Console.WriteLine(p.Index+" "+p.Position.ToString());
+                Console.WriteLine(p.Index + " " + p.Position.ToString());
             }
         }
 
@@ -141,11 +155,11 @@ namespace FluidSolver {
     }
 
     public class Particle {
-        public Particle (){
+        public Particle() {
 
         }
-        
-        public Particle(int inIndex, float inMass,Vector3 inPosition) {
+
+        public Particle(int inIndex, float inMass, Vector3 inPosition) {
             this.Index = inIndex;
             this.Mass = inMass;
             this.Position = inPosition;
@@ -171,5 +185,7 @@ namespace FluidSolver {
         public Vector3 ViscosityForceAcceleration { get; set; } = new Vector3();
 
         public Vector3 GravityAcceleration { get; set; } = new Vector3();
+
+        public Vector3 SurfaceTensionAcceleration { get; set; } = new Vector3();
     }
 }
--- FluidSolver/FluidSolver/Program.cs
+++ FluidSolver/FluidSolver/Program.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Numerics;
 
 namespace FluidSolver {
     static class Program {
         static void Main(string[] args) {
 
             Console.WriteLine("Start Generate Particles");
-            var fluidSolver = new FluidSolver(new Vector(100,100,100),0.01f);
+            var fluidSolver = new FluidSolver(new Vector3(100,100,100),0.01f);
             
             //fluidSolver.TestInitParticles();
         }
